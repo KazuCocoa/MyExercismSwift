34ac2cff07b3e601a0aa38377f0a532ff041a001
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-03-18 23:33:11 +0900

    remove unused logic

--- MyExercismSwiftTests/etl/Etl.swift
+++ MyExercismSwiftTests/etl/Etl.swift
@@ -1,13 +1,9 @@
 struct ETL {
-    static func transform(olds: [Int: Array<String>]) -> [String: Int] {
+    static func transform(olds: [Int: [String]]) -> [String: Int] {
         var trans = [String: Int]()
         olds.forEach { old in
             old.1.forEach { string in
-                if let count = trans[string.lowercaseString] {
-                    trans[string.lowercaseString] = count + old.0
-                } else {
-                    trans[string.lowercaseString] = old.0
-                }
+                trans[string.lowercaseString] = old.0
             }
         }
 
