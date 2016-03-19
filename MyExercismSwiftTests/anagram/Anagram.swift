962d7cc468aaa80333320185ddbea2682aa0499f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-03-20 00:40:37 +0900

    make more simple

--- MyExercismSwiftTests/anagram/Anagram.swift
+++ MyExercismSwiftTests/anagram/Anagram.swift
@@ -7,35 +7,13 @@ struct Anagram {
 
         comparedWords.forEach { comparedWord in
             let sortedCompared = comparedWord.lowercaseString.characters.sort()
-            if sortedBase.count == sortedCompared.count {
-                let zippedWords = zip(sortedBase, sortedCompared)
-                for (n, (c1, c2)) in zippedWords.enumerate() {
-                    if n < sortedBase.count {
-                        if c1 == c2 {
-                            if n == sortedBase.count - 1 {
-                                results.append(comparedWord)
-                            }
-                        } else {
-                            break
-                        }
-                    } else {
-                        break
-                    }
+            if sortedBase == sortedCompared {
+                if comparedWord.lowercaseString != word.lowercaseString {
+                    results.append(comparedWord)
                 }
             }
         }
 
-        return unique(results)
-    }
-
-    func unique(list: [String]) -> [String] {
-        var result: [String] = []
-        list.forEach { string in
-            if string.lowercaseString != word.lowercaseString {
-                result.append(string)
-            }
-        }
-
-        return result
+        return results
     }
 }
