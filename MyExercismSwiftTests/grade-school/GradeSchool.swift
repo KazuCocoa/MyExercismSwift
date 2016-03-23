0f5beb4e4efef94966224ea88c8ab3d7d098c7c0
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-03-24 07:34:43 +0900

    add gradeschool

---
+++ MyExercismSwiftTests/grade-school/GradeSchool.swift
@@ -0,0 +1,29 @@
+struct GradeSchool {
+    var roster = [Int: Set<String>]()
+
+    mutating func addStudent(name: String, grade: Int) {
+        if let students = roster[grade] {
+            var students = students
+            students.insert(name)
+            roster[grade] = students
+        } else {
+            roster[grade] = Set(arrayLiteral: name)
+        }
+    }
+
+    func studentsInGrade(grade: Int) -> Set<String> {
+        if let students = roster[grade] {
+            return students
+        } else {
+            return Set<String>()
+        }
+    }
+
+    var sortedRoster: [Int: Set<String>] {
+        var sortedRoster = [Int: Set<String>](minimumCapacity: roster.count)
+        for (grade, students) in roster {
+            sortedRoster[grade] = Set(students.sort())
+        }
+        return sortedRoster
+    }
+}
